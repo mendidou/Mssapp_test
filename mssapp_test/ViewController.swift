5ba6bcadf34a87316bb52d8b3c89c3af29df6a51
mendy <maouizerat@gmail.com>
2020-09-16 15:03:53 +0300

    Initial Commit

---
+++ mssapp_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mssapp_test
+//
+//  Created by mendy aouizerat  on 16/09/2020.
+//  Copyright © 2020 mendy aouizerat . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
